45f0ea2bca8cfd11bf87c2f5a2736413a5575fa7
Anna Atiskova <anna.atiskova@gmail.com>
2023-01-16 01:47:07 +0200

    fix catch

--- src/index.js
+++ src/index.js
@@ -18,29 +18,33 @@ const onCountryFormInput = event => {
     return;
   }
 
-   fetchCountries(searchedQuery).then(data => {
-    
-    if (data.length > 10) {
-      countryInfo.innerHTML = '';
-    countryList.innerHTML = '';
-      Notify.info('Too many matches found. Please enter a more specific name.');
-    } else if (data.length >= 2 && data.length <= 10) {
-      createCountryList(data);
-      countryInfo.innerHTML = '';
-    } else if (data.length === 1) {
-      createCountryCard(data[0]);
-      countryList.innerHTML = '';
-    }
-  })
-  .catch(err => {
-    switch (err.message) {
-      case '404': {
-        Notify.failure("Oops, there is no country with that name");
-        break;
+  fetchCountries(searchedQuery)
+    .then(data => {
+      if (data.length > 10) {
+        countryInfo.innerHTML = '';
+        countryList.innerHTML = '';
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (data.length >= 2 && data.length <= 10) {
+        createCountryList(data);
+        countryInfo.innerHTML = '';
+      } else if (data.length === 1) {
+        createCountryCard(data[0]);
+        countryList.innerHTML = '';
       }
-    }
-  })
-};  
+    })
+    .catch(err => {
+      switch (err.message) {
+        case '404': {
+          Notify.failure('Oops, there is no country with that name');
+          countryInfo.innerHTML = '';
+          countryList.innerHTML = '';
+          break;
+        }
+      }
+    });
+};
 
 function createCountryList(data) {
   const markup = data
@@ -55,7 +59,6 @@ function createCountryList(data) {
 }
 
 function createCountryCard(data) {
-
   const markup = `
       <div class="country-list-item">
       <img src="${data.flags.svg}" alt="flag" height="15px">
@@ -63,8 +66,12 @@ function createCountryCard(data) {
       </div>
       <ul class="country-list">
       <li><p class="country-list-text"><b>Capital</b>: ${data.capital}</p></li>
-      <li><p class="country-list-text"><b>Population</b>: ${data.population}</p></li>
-      <li><p class="country-list-text"><b>Languages</b>: ${Object.values(data.languages)
+      <li><p class="country-list-text"><b>Population</b>: ${
+        data.population
+      }</p></li>
+      <li><p class="country-list-text"><b>Languages</b>: ${Object.values(
+        data.languages
+      )
         .map(el => el.name)
         .join(', ')}</p></li> 
     </ul>`;
